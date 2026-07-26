0f50ebbceca12eb684265a88d18f00a59e84c1e2
ivettecarolina83 <ivettecarolina83@gmail.com>
2026-07-26 11:49:24 -0400

    Adjust timing for fluid intro

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ if (brandIntro) {
     brandIntro.addEventListener('animationend', (event) => {
       if (event.animationName === 'intro-overlay-out') finishBrandIntro();
     });
-    window.setTimeout(finishBrandIntro, 4700);
+    window.setTimeout(finishBrandIntro, 4100);
   }
 }
 const menuButton = document.querySelector('.menu-toggle');
