b7648391dcbe41a0b7a2f0e2e887d185f2e89d54
ivettecarolina83 <ivettecarolina83@gmail.com>
2026-07-26 11:55:20 -0400

    Ajustar duración de la presentación animada

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ if (brandIntro) {
     brandIntro.addEventListener('animationend', (event) => {
       if (event.animationName === 'intro-overlay-out') finishBrandIntro();
     });
-    window.setTimeout(finishBrandIntro, 4100);
+    window.setTimeout(finishBrandIntro, 5300);
   }
 }
 const menuButton = document.querySelector('.menu-toggle');
